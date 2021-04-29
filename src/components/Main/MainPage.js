2f15a267436b75394ceb743dd1d2728ee5e367b4
Artur Kempny <artur.kempny@gmail.com>
2021-04-29 11:36:55 +0200

    Create initial project

---
+++ src/components/Main/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import InputField from '../UI/Forms/InputField'
+
+const MainPage = () => {
+    return (
+        <div>
+            strona główna
+            <InputField />
+        </div>
+    )
+}
+
+export default MainPage
---
+++ src/data/userform.js
@@ -0,0 +1,37 @@
+export const user = [
+    {
+        id: 1,
+        name: "name",
+        title: "your name...",
+        type: "text",
+        id_field: "nameid"
+    },
+    {
+        id: 2,
+        name: "lastname",
+        title: "your lastname...",
+        type: "text",
+        id_field: "lastnameid"
+    },
+    {
+        id: 3,
+        name: "email",
+        title: "your email...",
+        type: "email",
+        id_field: "emailid"
+    },
+    {
+        id: 4,
+        name: "password",
+        title: "password...",
+        type: "password",
+        id_field: "posswordid"
+    },
+    {
+        id: 5,
+        name: "repeatpassword",
+        title: "repeat password...",
+        type: "password",
+        id_field: "repeatpasswordid"
+    },
+]
